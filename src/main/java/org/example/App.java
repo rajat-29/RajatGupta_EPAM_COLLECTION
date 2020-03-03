83f19d5214900c3523c7868fcf3ad60e6da8b1b6
rajat-29 <rajat295gupta@gmail.com>
2020-03-03 14:29:58 +0530

    collection

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,30 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class App
 {
+    private static final Logger LOGGER= Logger.getLogger(App.class.getName());
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        CustomList<Integer> newList = new CustomList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        LOGGER.info("Enter Size : ");
+        int n;
+        n = sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            newList.add(sc.nextInt());
+        }
+        newList.display();
+        LOGGER.info("Enter index you want to remove :");
+        newList.remove(sc.nextInt());
+        newList.display();
+        LOGGER.info("Enter number you want to add :");
+        newList.add(sc.nextInt());
+        newList.display();
+        System.out.println(newList.get(3));
+        newList.display();
     }
 }
